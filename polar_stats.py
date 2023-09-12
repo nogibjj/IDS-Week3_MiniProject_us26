6d9f6be7ea253d44924e978e12551939f45c7fba
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-12 03:16:28 -0400

    Update polar_stats.py

--- polar_stats.py
+++ polar_stats.py
@@ -12,3 +12,21 @@ def stats_mode(dataset):
 
 def stats_std(dataset):
     return dataset["No of student per staff"].std()
+
+
+def report(data):
+    result = {
+        "mean": (stats_mean(data)),
+        "median": (stats_median(data)),
+        "std_dev": (stats_std(data)),
+        "mode": ((stats_mode(data)[0])),
+    }
+
+    return result
+
+
+def create_summary(data, file_path="Generated summary report.md"):
+    with open(file_path, "w", encoding="utf-8") as f:
+        for key, value in report(data).items():
+            f.write("%s:%s\n" % (key, value))
+            f.write('\n')
